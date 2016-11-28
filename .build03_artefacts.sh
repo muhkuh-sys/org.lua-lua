4b41264f4f47161cf4689652a47e717b1c6dd0e7
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-11-28 10:04:58 +0100

    Move the installer data into the components folder. Add the module name to the install path.

--- .build03_artefacts.sh
+++ .build03_artefacts.sh
@@ -7,24 +7,24 @@ set -e
 #
 rm -rf build
 mkdir build
-mkdir build/org.lua-lua51
-mkdir build/org.lua-lua52
-mkdir build/org.lua-lua53
+mkdir build/org.lua.lua-lua51
+mkdir build/org.lua.lua-lua52
+mkdir build/org.lua.lua-lua53
 
-pushd build/org.lua-lua51
-cmake -DCMAKE_INSTALL_PREFIX="" ../../installer/lua5.1
+pushd build/org.lua.lua-lua51
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.1/installer
 make
 make package
 popd
 
-pushd build/org.lua-lua52
-cmake -DCMAKE_INSTALL_PREFIX="" ../../installer/lua5.2
+pushd build/org.lua.lua-lua52
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.2/installer
 make
 make package
 popd
 
-pushd build/org.lua-lua53
-cmake -DCMAKE_INSTALL_PREFIX="" ../../installer/lua5.3
+pushd build/org.lua.lua-lua53
+cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.3/installer
 make
 make package
 popd
