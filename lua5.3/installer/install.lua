625271e4dc6ac89ac65e50ecf62712cd42907019
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-11-28 10:22:21 +0100

    Change the windows 64bit installation folder to "windows_x64". Install the headers to "include/lua_x.y/".

--- lua5.3/installer/install.lua
+++ lua5.3/installer/install.lua
@@ -6,11 +6,17 @@ if strPlatform=='Windows_x86' then
 	t:install('windows_x86/bin/lua5.3.exe', '${install.executable}')
 	t:install('windows_x86/bin/wlua5.3.exe', '${install.executable}')
 	t:install('windows_x86/bin/lua5.3.dll', '${install.sharedobject}')
+	t:install('windows_x86/include', '${install_dev_include}')
+	t:install('windows_x86/lib', '${install_dev_lib}')
+	t:install('windows_x86/cmake', '${install_dev_cmake}')
 
 elseif strPlatform=='Windows_x64' then
 	t:install('windows_x64/bin/lua5.3.exe', '${install.executable}')
 	t:install('windows_x64/bin/wlua5.3.exe', '${install.executable}')
 	t:install('windows_x64/bin/lua5.3.dll', '${install.sharedobject}')
+	t:install('windows_x64/include', '${install_dev_include}')
+	t:install('windows_x64/lib', '${install_dev_lib}')
+	t:install('windows_x64/cmake', '${install_dev_cmake}')
 
 end
 
