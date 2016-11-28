637582c1dfc0d07faa4488f37518a884c7b1af2b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-11-28 17:12:24 +0100

    Remove LUA5.1 and LUA5.2 .

--- .build03_artefacts.sh
+++ .build03_artefacts.sh
@@ -7,21 +7,7 @@ set -e
 #
 rm -rf build
 mkdir build
-mkdir build/org.lua.lua-lua51
-mkdir build/org.lua.lua-lua52
 mkdir build/org.lua.lua-lua53
-
-pushd build/org.lua.lua-lua51
-cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.1/installer
-make
-make package
-popd
-
-pushd build/org.lua.lua-lua52
-cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.2/installer
-make
-make package
-popd
 
 pushd build/org.lua.lua-lua53
 cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.3/installer
