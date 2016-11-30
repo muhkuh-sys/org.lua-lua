40e5dba8446c3a1a45690d1e002d14f9bb3b4e95
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-11-30 13:51:02 +0100

    Mark all paths as directories.

--- lua5.2/installer/install.lua
+++ lua5.2/installer/install.lua
@@ -1,22 +1,20 @@
 local t = ...
 
-local strPlatform = 'Windows_x64'
-
 if strPlatform=='Windows_x86' then
-	t:install('windows_x86/bin/lua5.2.exe', '${install.executable}')
-	t:install('windows_x86/bin/wlua5.2.exe', '${install.executable}')
-	t:install('windows_x86/bin/lua5.2.dll', '${install.sharedobject}')
-	t:install('windows_x86/include', '${install_dev_include}')
-	t:install('windows_x86/lib', '${install_dev_lib}')
-	t:install('windows_x86/cmake', '${install_dev_cmake}')
+	t:install('windows_x86/bin/lua5.2.exe',  '${install_executables}/')
+	t:install('windows_x86/bin/wlua5.2.exe', '${install_executables}/')
+	t:install('windows_x86/bin/lua5.2.dll',  '${install_shared_objects}/')
+	t:install('windows_x86/include',         '${install_dev_include}/')
+	t:install('windows_x86/lib',             '${install_dev_lib}/')
+	t:install('windows_x86/cmake',           '${install_dev_cmake}/')
 
 elseif strPlatform=='Windows_x64' then
-	t:install('windows_x64/bin/lua5.2.exe', '${install.executable}')
-	t:install('windows_x64/bin/wlua5.2.exe', '${install.executable}')
-	t:install('windows_x64/bin/lua5.2.dll', '${install.sharedobject}')
-	t:install('windows_x64/include', '${install_dev_include}')
-	t:install('windows_x64/lib', '${install_dev_lib}')
-	t:install('windows_x64/cmake', '${install_dev_cmake}')
+	t:install('windows_x64/bin/lua5.2.exe',  '${install_executables}/')
+	t:install('windows_x64/bin/wlua5.2.exe', '${install_executables}/')
+	t:install('windows_x64/bin/lua5.2.dll',  '${install_shared_objects}/')
+	t:install('windows_x64/include',         '${install_dev_include}/')
+	t:install('windows_x64/lib',             '${install_dev_lib}/')
+	t:install('windows_x64/cmake',           '${install_dev_cmake}/')
 
 end
 
