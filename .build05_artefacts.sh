e42a7f439f075586855d4dd62a8f0a48ee43753d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-04 13:07:05 +0100

    Unpack the build archives and add all to the artifact.

--- .build05_artefacts.sh
+++ .build05_artefacts.sh
@@ -5,9 +5,36 @@ set -e
 #
 # Build all artefacts.
 #
-rm -rf build
-mkdir build
-mkdir build/org.lua.lua-lua51
+pushd build
+rm -rf org.lua.lua-lua51
+rm -rf windows_32
+rm -rf windows_64
+rm -rf ubuntu_1204_32
+rm -rf ubuntu_1204_64
+rm -rf ubuntu_1404_32
+rm -rf ubuntu_1404_64
+rm -rf ubuntu_1604_32
+rm -rf ubuntu_1604_64
+
+mkdir org.lua.lua-lua51
+mkdir windows_32
+mkdir windows_64
+mkdir ubuntu_1204_32
+mkdir ubuntu_1204_64
+mkdir ubuntu_1404_32
+mkdir ubuntu_1404_64
+mkdir ubuntu_1604_32
+mkdir ubuntu_1604_64
+
+tar --extract --directory windows_32 --file build_windows_32.tar.gz --gzip
+tar --extract --directory windows_64 --file build_windows_64.tar.gz --gzip
+tar --extract --directory ubuntu_1204_32 --file build_ubuntu_1204_32.tar.gz --gzip
+tar --extract --directory ubuntu_1204_64 --file build_ubuntu_1204_64.tar.gz --gzip
+tar --extract --directory ubuntu_1404_32 --file build_ubuntu_1404_32.tar.gz --gzip
+tar --extract --directory ubuntu_1404_64 --file build_ubuntu_1404_64.tar.gz --gzip
+tar --extract --directory ubuntu_1604_32 --file build_ubuntu_1604_32.tar.gz --gzip
+tar --extract --directory ubuntu_1604_64 --file build_ubuntu_1604_64.tar.gz --gzip
+popd
 
 pushd build/org.lua.lua-lua51
 cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.1/installer
