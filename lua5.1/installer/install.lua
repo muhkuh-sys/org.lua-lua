f0fd5e1962fb865e8d916da7548e4a56077f8b9e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-05 23:33:50 +0100

    Install the components for all operating systems.

--- lua5.1/installer/install.lua
+++ lua5.1/installer/install.lua
@@ -1,20 +1,77 @@
 local t = ...
+local strPlatform = t:get_platform()
 
-if strPlatform=='Windows_x86' then
-	t:install('windows_x86/bin/lua5.1.exe',  '${install_executables}/')
-	t:install('windows_x86/bin/wlua5.1.exe', '${install_executables}/')
-	t:install('windows_x86/bin/lua5.1.dll',  '${install_shared_objects}/')
-	t:install('windows_x86/include',         '${install_dev_include}/')
-	t:install('windows_x86/lib',             '${install_dev_lib}/')
-	t:install('windows_x86/cmake',           '${install_dev_cmake}/')
+if strPlatform=='windows_32' then
+	t:install('windows_32/bin/lua5.1.exe',    '${install_executables}/')
+	t:install('windows_32/bin/wlua5.1.exe',   '${install_executables}/')
+	t:install('windows_32/bin/lua5.1.dll',    '${install_shared_objects}/')
+	t:install('windows_32/include',           '${install_dev_include}/')
+	t:install('windows_32/lib',               '${install_dev_lib}/')
+	t:install('windows_32/cmake',             '${install_dev_cmake}/')
 
-elseif strPlatform=='Windows_x64' then
-	t:install('windows_x64/bin/lua5.1.exe',  '${install_executables}/')
-	t:install('windows_x64/bin/wlua5.1.exe', '${install_executables}/')
-	t:install('windows_x64/bin/lua5.1.dll',  '${install_shared_objects}/')
-	t:install('windows_x64/include',         '${install_dev_include}/')
-	t:install('windows_x64/lib',             '${install_dev_lib}/')
-	t:install('windows_x64/cmake',           '${install_dev_cmake}/')
+elseif strPlatform=='windows_64' then
+	t:install('windows_64/bin/lua5.1.exe',    '${install_executables}/')
+	t:install('windows_64/bin/wlua5.1.exe',   '${install_executables}/')
+	t:install('windows_64/bin/lua5.1.dll',    '${install_shared_objects}/')
+	t:install('windows_64/include',           '${install_dev_include}/')
+	t:install('windows_64/lib',               '${install_dev_lib}/')
+	t:install('windows_64/cmake',             '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1204_32' then
+	t:install('ubuntu_1204_32/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1204_32/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1204_32/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1204_32/include',       '${install_dev_include}/')
+	t:install('ubuntu_1204_32/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1204_64' then
+	t:install('ubuntu_1204_64/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1204_64/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1204_64/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1204_64/include',       '${install_dev_include}/')
+	t:install('ubuntu_1204_64/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1404_32' then
+	t:install('ubuntu_1404_32/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1404_32/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1404_32/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1404_32/include',       '${install_dev_include}/')
+	t:install('ubuntu_1404_32/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1404_64' then
+	t:install('ubuntu_1404_64/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1404_64/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1404_64/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1404_64/include',       '${install_dev_include}/')
+	t:install('ubuntu_1404_64/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1604_32' then
+	t:install('ubuntu_1604_32/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1604_32/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1604_32/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1604_32/include',       '${install_dev_include}/')
+	t:install('ubuntu_1604_32/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1604_64' then
+	t:install('ubuntu_1604_64/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1604_64/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1604_64/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1604_64/include',       '${install_dev_include}/')
+	t:install('ubuntu_1604_64/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1610_32' then
+	t:install('ubuntu_1610_32/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1610_32/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1610_32/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1610_32/include',       '${install_dev_include}/')
+	t:install('ubuntu_1610_32/cmake',         '${install_dev_cmake}/')
+
+elseif strPlatform=='ubuntu_1610_64' then
+	t:install('ubuntu_1610_64/bin/lua5.1',    '${install_executables}/')
+	t:install('ubuntu_1610_64/bin/wlua5.1',   '${install_executables}/')
+	t:install('ubuntu_1610_64/bin/lua5.1.so', '${install_shared_objects}/')
+	t:install('ubuntu_1610_64/include',       '${install_dev_include}/')
+	t:install('ubuntu_1610_64/cmake',         '${install_dev_cmake}/')
 
 end
 
