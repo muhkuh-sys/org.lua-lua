b1879b75937315c29a5bbab0e8b70d5f16a31cbc
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-17 17:02:00 +0100

    Rename the linux build folders to "build_linux32" and "build_linux64".

--- .build03_linux32.sh
+++ .build03_linux32.sh
@@ -5,9 +5,9 @@ set -e
 #
 # Build the 32bit version.
 #
-rm -rf build32
-mkdir build32
-pushd build32
+rm -rf build_linux32
+mkdir build_linux32
+pushd build_linux32
 cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
 make
 popd
--- .build04_linux64.sh
+++ .build04_linux64.sh
@@ -5,9 +5,9 @@ set -e
 #
 # Build the 64bit version.
 #
-rm -rf build64
-mkdir build64
-pushd build64
+rm -rf build_linux64
+mkdir build_linux64
+pushd build_linux64
 cmake -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 ..
 make
 popd
