2e9b050e51f6a07f66ec52fd4f6701f48a421df1
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2016-12-18 14:19:50 +0100

    Add the module name to the full package ID. Install the Ubuntu 16.10 files.

--- .build05_artefacts.sh
+++ .build05_artefacts.sh
@@ -6,7 +6,7 @@ set -e
 # Build all artefacts.
 #
 pushd build
-rm -rf org.lua-lua5.1
+rm -rf org.lua.lua-lua5.1
 rm -rf windows_x86
 rm -rf windows_x86_64
 rm -rf ubuntu_1204_x86
@@ -15,8 +15,10 @@ rm -rf ubuntu_1404_x86
 rm -rf ubuntu_1404_x86_64
 rm -rf ubuntu_1604_x86
 rm -rf ubuntu_1604_x86_64
+rm -rf ubuntu_1610_x86
+rm -rf ubuntu_1610_x86_64
 
-mkdir org.lua-lua5.1
+mkdir org.lua.lua-lua5.1
 mkdir windows_x86
 mkdir windows_x86_64
 mkdir ubuntu_1204_x86
@@ -25,6 +27,8 @@ mkdir ubuntu_1404_x86
 mkdir ubuntu_1404_x86_64
 mkdir ubuntu_1604_x86
 mkdir ubuntu_1604_x86_64
+mkdir ubuntu_1610_x86
+mkdir ubuntu_1610_x86_64
 
 tar --extract --directory windows_x86 --file build_windows_x86.tar.gz --gzip
 tar --extract --directory windows_x86_64 --file build_windows_x86_64.tar.gz --gzip
@@ -34,9 +38,11 @@ tar --extract --directory ubuntu_1404_x86 --file build_ubuntu_1404_x86.tar.gz --
 tar --extract --directory ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64.tar.gz --gzip
 tar --extract --directory ubuntu_1604_x86 --file build_ubuntu_1604_x86.tar.gz --gzip
 tar --extract --directory ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64.tar.gz --gzip
+tar --extract --directory ubuntu_1610_x86 --file build_ubuntu_1610_x86.tar.gz --gzip
+tar --extract --directory ubuntu_1610_x86_64 --file build_ubuntu_1610_x86_64.tar.gz --gzip
 popd
 
-pushd build/org.lua-lua5.1
+pushd build/org.lua.lua-lua5.1
 cmake -DCMAKE_INSTALL_PREFIX="" ../../lua5.1/installer
 make
 make package
