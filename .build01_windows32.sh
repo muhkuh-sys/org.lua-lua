1c5765de52d488b682c6cefde0f066d479a3eaa5
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-14 17:04:49 +0100

    Move the build folders below "build".

--- .build01_windows32.sh
+++ .build01_windows32.sh
@@ -5,9 +5,9 @@ set -e
 #
 # Build the windows32 version.
 #
-rm -rf build_windows32
-mkdir build_windows32
-pushd build_windows32
-cmake -DCMAKE_INSTALL_PREFIX="" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres ..
+rm -rf build/windows32
+mkdir -p build/windows32
+pushd build/windows32
+cmake -DCMAKE_INSTALL_PREFIX="" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres ../..
 make
 popd
