3d17ba8cfe9a050cca40edf48539459767dd04b4
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-16 11:09:37 +0100

    Place the build folder below the "build" directory.

--- .build02_windows64.sh
+++ .build02_windows64.sh
@@ -5,9 +5,9 @@ set -e
 #
 # Build the windows64 version.
 #
-rm -rf build_windows64
-mkdir build_windows64
-pushd build_windows64
-cmake -DCMAKE_INSTALL_PREFIX="" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ..
+rm -rf build/windows64
+mkdir build/windows64
+pushd build/windows64
+cmake -DCMAKE_INSTALL_PREFIX="" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ../..
 make
 popd
