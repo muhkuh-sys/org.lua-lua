026ba3b6acde7c055b285f84ec91730a3aa2c99e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-05-02 10:47:01 +0200

    Drop support for Ubuntu 12.04 . Add support for Ubuntu 17.04 .

--- lua5.3/installer/install.lua
+++ lua5.3/installer/install.lua
@@ -1,5 +1,6 @@
 local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
+local tResult
 
 if strDistId=='windows' then
   if strCpuArch=='x86' then
@@ -9,6 +10,7 @@ if strDistId=='windows' then
     t:install_dev('windows_x86/dev/include',          '${install_dev_include}/')
     t:install_dev('windows_x86/dev/lib',              '${install_dev_lib}/')
     t:install_dev('windows_x86/dev/cmake',            '${install_dev_cmake}/')
+    tResult = true
   elseif strCpuArch=='x86_64' then
     t:install(    'windows_x86_64/lua5.3.exe',        '${install_executables}/')
     t:install(    'windows_x86_64/wlua5.3.exe',       '${install_executables}/')
@@ -16,37 +18,25 @@ if strDistId=='windows' then
     t:install_dev('windows_x86_64/dev/include',       '${install_dev_include}/')
     t:install_dev('windows_x86_64/dev/lib',           '${install_dev_lib}/')
     t:install_dev('windows_x86_64/dev/cmake',         '${install_dev_cmake}/')
+    tResult = true
   end
 
 elseif strDistId=='ubuntu' then
-  if strDistVersion=='12.04' then
-    if strCpuArch=='x86' then
-      t:install(    'ubuntu_1204_x86/lua5.3',         '${install_executables}/')
-      t:install(    'ubuntu_1204_x86/wlua5.3',        '${install_executables}/')
-      t:install(    'ubuntu_1204_x86/lua5.3.so',      '${install_shared_objects}/')
-      t:install_dev('ubuntu_1204_x86/dev/include',    '${install_dev_include}/')
-      t:install_dev('ubuntu_1204_x86/dev/cmake',      '${install_dev_cmake}/')
-    elseif strCpuArch=='x86_64' then
-      t:install(    'ubuntu_1204_x86_64/lua5.3',      '${install_executables}/')
-      t:install(    'ubuntu_1204_x86_64/wlua5.3',     '${install_executables}/')
-      t:install(    'ubuntu_1204_x86_64/lua5.3.so',   '${install_shared_objects}/')
-      t:install_dev('ubuntu_1204_x86_64/dev/include', '${install_dev_include}/')
-      t:install_dev('ubuntu_1204_x86_64/dev/cmake',   '${install_dev_cmake}/')
-    end
-
-  elseif strDistVersion=='14.04' then
+  if strDistVersion=='14.04' then
     if strCpuArch=='x86' then
       t:install(    'ubuntu_1404_x86/lua5.3',         '${install_executables}/')
       t:install(    'ubuntu_1404_x86/wlua5.3',        '${install_executables}/')
       t:install(    'ubuntu_1404_x86/lua5.3.so',      '${install_shared_objects}/')
       t:install_dev('ubuntu_1404_x86/dev/include',    '${install_dev_include}/')
       t:install_dev('ubuntu_1404_x86/dev/cmake',      '${install_dev_cmake}/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install(    'ubuntu_1404_x86_64/lua5.3',      '${install_executables}/')
       t:install(    'ubuntu_1404_x86_64/wlua5.3',     '${install_executables}/')
       t:install(    'ubuntu_1404_x86_64/lua5.3.so',   '${install_shared_objects}/')
       t:install_dev('ubuntu_1404_x86_64/dev/include', '${install_dev_include}/')
       t:install_dev('ubuntu_1404_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      tResult = true
     end
 
   elseif strDistVersion=='16.04' then
@@ -56,12 +46,14 @@ elseif strDistId=='ubuntu' then
       t:install(    'ubuntu_1604_x86/lua5.3.so',      '${install_shared_objects}/')
       t:install_dev('ubuntu_1604_x86/dev/include',    '${install_dev_include}/')
       t:install_dev('ubuntu_1604_x86/dev/cmake',      '${install_dev_cmake}/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install(    'ubuntu_1604_x86_64/lua5.3',      '${install_executables}/')
       t:install(    'ubuntu_1604_x86_64/wlua5.3',     '${install_executables}/')
       t:install(    'ubuntu_1604_x86_64/lua5.3.so',   '${install_shared_objects}/')
       t:install_dev('ubuntu_1604_x86_64/dev/include', '${install_dev_include}/')
       t:install_dev('ubuntu_1604_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      tResult = true
     end
 
   elseif strDistVersion=='16.10' then
@@ -71,14 +63,33 @@ elseif strDistId=='ubuntu' then
       t:install(    'ubuntu_1610_x86/lua5.3.so',      '${install_shared_objects}/')
       t:install_dev('ubuntu_1610_x86/dev/include',    '${install_dev_include}/')
       t:install_dev('ubuntu_1610_x86/dev/cmake',      '${install_dev_cmake}/')
+      tResult = true
     elseif strCpuArch=='x86_64' then
       t:install(    'ubuntu_1610_x86_64/lua5.3',      '${install_executables}/')
       t:install(    'ubuntu_1610_x86_64/wlua5.3',     '${install_executables}/')
       t:install(    'ubuntu_1610_x86_64/lua5.3.so',   '${install_shared_objects}/')
       t:install_dev('ubuntu_1610_x86_64/dev/include', '${install_dev_include}/')
       t:install_dev('ubuntu_1610_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      tResult = true
+    end
+
+  elseif strDistVersion=='17.04' then
+    if strCpuArch=='x86' then
+      t:install(    'ubuntu_1704_x86/lua5.3',         '${install_executables}/')
+      t:install(    'ubuntu_1704_x86/wlua5.3',        '${install_executables}/')
+      t:install(    'ubuntu_1704_x86/lua5.3.so',      '${install_shared_objects}/')
+      t:install_dev('ubuntu_1704_x86/dev/include',    '${install_dev_include}/')
+      t:install_dev('ubuntu_1704_x86/dev/cmake',      '${install_dev_cmake}/')
+      tResult = true
+    elseif strCpuArch=='x86_64' then
+      t:install(    'ubuntu_1704_x86_64/lua5.3',      '${install_executables}/')
+      t:install(    'ubuntu_1704_x86_64/wlua5.3',     '${install_executables}/')
+      t:install(    'ubuntu_1704_x86_64/lua5.3.so',   '${install_shared_objects}/')
+      t:install_dev('ubuntu_1704_x86_64/dev/include', '${install_dev_include}/')
+      t:install_dev('ubuntu_1704_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      tResult = true
     end
   end
 end
 
-return true
+return tResult
