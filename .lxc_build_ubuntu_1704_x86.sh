d064993a8c24ea9011017bf6ecf11cb8dce03072
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-05-02 10:44:48 +0200

    Drop support for Ubuntu 12.04 . Add support for Ubuntu 17.04 .

--- .lxc_build_ubuntu_1704_x86.sh
+++ .lxc_build_ubuntu_1704_x86.sh
@@ -13,7 +13,7 @@ PRJDIR=`pwd`
 mkdir -p ${PRJDIR}/build
 
 # Start the container and mount the project folder.
-lxc launch mbs-ubuntu-1204-x64 ${CONTAINER} -c security.privileged=true
+lxc launch mbs-ubuntu-1704-x86 ${CONTAINER} -c security.privileged=true
 lxc config device add ${CONTAINER} projectDir disk source=${PRJDIR} path=/tmp/work
 sleep 5
 
@@ -23,10 +23,10 @@ lxc exec ${CONTAINER} -- bash -c 'apt-get update --assume-yes'
 # Install the project specific packages.
 lxc exec ${CONTAINER} -- bash -c 'apt-get install --assume-yes imagemagick icoutils'
 
-# Build the 64bit version.
+# Build the 32bit version.
 lxc exec ${CONTAINER} -- bash -c 'cd /tmp/work && bash .build03_linux.sh'
-lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1204_x86_64.tar.gz --gzip --directory /tmp/work/build/linux/lua5.2/lua/install .'
-lxc exec ${CONTAINER} -- bash -c 'chown `stat -c %u:%g /tmp/work` /tmp/work/build/build_ubuntu_1204_x86_64.tar.gz'
+lxc exec ${CONTAINER} -- bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1704_x86.tar.gz --gzip --directory /tmp/work/build/linux/lua5.2/lua/install .'
+lxc exec ${CONTAINER} -- bash -c 'chown `stat -c %u:%g /tmp/work` /tmp/work/build/build_ubuntu_1704_x86.tar.gz'
 
 # Stop and remove the container.
 lxc stop ${CONTAINER}
