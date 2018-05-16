48a6da07e1b0b7a314f3d156e805499048cc7a42
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-05-16 15:49:22 +0200

    Overwrite the platform ID for Windows builds.

--- .build01_windows32.sh
+++ .build01_windows32.sh
@@ -8,6 +8,6 @@ set -e
 rm -rf build/windows32
 mkdir -p build/windows32
 pushd build/windows32
-cmake -DCMAKE_INSTALL_PREFIX="" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres ../..
-make
+cmake -DCMAKE_INSTALL_PREFIX="" -DJONCHKI_PLATFORM_DIST_ID="windows" -DJONCHKI_PLATFORM_DIST_VERSION="" -DJONCHKI_PLATFORM_CPU_ARCH="x86" -DImageMagick_convert_EXECUTABLE=/usr/bin/convert -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_C_COMPILER=i686-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++ -DCMAKE_RC_COMPILER=i686-w64-mingw32-windres ../..
+make pack
 popd
