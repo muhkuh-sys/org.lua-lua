e36669e29b5bc6eab1e270d300a03e85b3e650b5
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-12 21:26:25 +0200

    Remove obsolete "end"s.

--- lua5.1/installer/install_windows.lua
+++ lua5.1/installer/install_windows.lua
@@ -3,14 +3,13 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' then
-    t:install(    'lua5.1.exe',           '${install_executables}/')
-    t:install(    'wlua5.1.exe',          '${install_executables}/')
-    t:install(    'lua5.1.dll',           '${install_shared_objects}/')
-    t:install_dev('dev/include',          '${install_dev_include}/')
-    t:install_dev('dev/lib',              '${install_dev_lib}/')
-    t:install_dev('dev/cmake',            '${install_dev_cmake}/')
-    tResult = true
-  end
+  t:install(    'lua5.1.exe',           '${install_executables}/')
+  t:install(    'wlua5.1.exe',          '${install_executables}/')
+  t:install(    'lua5.1.dll',           '${install_shared_objects}/')
+  t:install_dev('dev/include',          '${install_dev_include}/')
+  t:install_dev('dev/lib',              '${install_dev_lib}/')
+  t:install_dev('dev/cmake',            '${install_dev_cmake}/')
+  tResult = true
 end
 
 return tResult
