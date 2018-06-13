dc16405022d496ea13cbbd57ef0e41665592d0fa
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-13 11:20:49 +0200

    Update the lua version to 5.3 in the installer scripts.

--- lua5.3/installer/install_windows.lua
+++ lua5.3/installer/install_windows.lua
@@ -3,9 +3,9 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' then
-  t:install(    'lua5.2.exe',           '${install_executables}/')
-  t:install(    'wlua5.2.exe',          '${install_executables}/')
-  t:install(    'lua5.2.dll',           '${install_shared_objects}/')
+  t:install(    'lua5.3.exe',           '${install_executables}/')
+  t:install(    'wlua5.3.exe',          '${install_executables}/')
+  t:install(    'lua5.3.dll',           '${install_shared_objects}/')
   t:install_dev('dev/include',          '${install_dev_include}/')
   t:install_dev('dev/lib',              '${install_dev_lib}/')
   t:install_dev('dev/cmake',            '${install_dev_cmake}/')
